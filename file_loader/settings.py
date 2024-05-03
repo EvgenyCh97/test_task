ba2549ef0cece5f0fb9ca63bdf3e14efdaa7c018
Евгений <19hflf97@rambler.ru>
2024-05-03 12:42:50 +0300

    add 127.0.0.1 to ALLOWED_HOSTS

--- file_loader/settings.py
+++ file_loader/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['kubernetes.docker.internal']
+ALLOWED_HOSTS = ['127.0.0.1', 'kubernetes.docker.internal']
 
 
 # Application definition
