f6c56f7780373ae5eff8b9be86981c0d8bb6a5ce
Евгений <19hflf97@rambler.ru>
2024-05-03 12:46:01 +0300

    update permissions.py and remove admin/ url from urls

--- api/permissions.py
+++ api/permissions.py
@@ -4,8 +4,7 @@ from rest_framework.permissions import BasePermission
 class IsSameUser(BasePermission):
 
     def has_permission(self, request, view):
-        return request.user.is_authenticated or request.method in ['GET',
-                                                                   'POST']
+        return request.method in ['GET', 'POST']
 
     def has_object_permission(self, request, view, obj):
         if (request.method in ['PATCH', 'PUT', 'DELETE'] and
