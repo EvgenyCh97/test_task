eb2fc0504aeb7aac55f9fc158d61f157537dfba1
Евгений <19hflf97@rambler.ru>
2024-05-08 15:52:14 +0300

    remove test db from settings.py

--- root_app/settings.py
+++ root_app/settings.py
@@ -87,10 +87,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    'test': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'test_db.sqlite3',
-    },
 }
 
 
